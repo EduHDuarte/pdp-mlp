18cf757bf2161cae6fcb158eb4df34d01f880cd0
caracalla <eahduarte@inf.ufrgs.br>
2025-10-05 12:58:03 -0300

    Revert "Reduce image dimensionality"
    This reverts commit 8b7a5a12382619d8acf57e993f1b1bd9507a5934.

--- util/img.c
+++ util/img.c
@@ -5,8 +5,6 @@
 
 #define MAXCHAR 10000
 
-
-
 Img** csv_to_imgs(char* file_string, int number_of_imgs) {
 	FILE *fp;
 	Img** imgs = malloc(number_of_imgs * sizeof(Img*));
@@ -27,14 +25,7 @@ Img** csv_to_imgs(char* file_string, int number_of_imgs) {
 			if (j == 0) {
 				imgs[i]->label = atoi(token);
 			} else {
-				int orig_row = (j-1) / 28;
-				int orig_col = (j-1) % 28;
-
-				if (orig_row % 2 == 0 && orig_col % 2 == 0) {
-					int new_row = orig_row / 2;
-					int new_col = orig_col / 2;
-					imgs[i]->img_data->entries[new_row][new_col] = atoi(token) / 256.0;
-				}
+				imgs[i]->img_data->entries[(j-1) / 28][(j-1) % 28] = atoi(token) / 256.0;
 			}
 			token = strtok(NULL, ",");
 			j++;
